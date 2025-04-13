51f215bb29c93c069dc3653d63e374316a41d5e8
Brian Kristofer Perez <161929458+Brian-Kristofer-Perez@users.noreply.github.com>
2025-04-13 19:35:35 +0800

    Update DB.py

--- app/backend/Database/DB.py
+++ app/backend/Database/DB.py
@@ -3,6 +3,10 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
+# change this to your own local database account!
+# In future projects, let's use environment variables
+# because hardcoding this is a mortal sin
 user = "root"
 password = "12345678"
 engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@127.0.0.1/DahonDB")
