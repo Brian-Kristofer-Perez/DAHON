44efddbef58bcddb0d07293a0c8b25fc7a7c4060
Brian Kristofer Perez <161929458+Brian-Kristofer-Perez@users.noreply.github.com>
2025-05-09 23:15:44 +0800

    set cloud database credentials

--- app/backend/Database/DB.py
+++ app/backend/Database/DB.py
@@ -4,18 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-
-# user = "avnadmin"
-# password = os.getenv("DB_PASSWORD")
-# port = "14893"
-# database_name = "defaultdb"
-# database_address = "mysql-a014630-project-159c.d.aivencloud.com"
-
-user = "root"
-password = "12345678"
-port = "3306"
-database_name = "dahondb"
-database_address = "127.0.0.1"
+user = "avnadmin"
+password = os.getenv("DB_PASSWORD")
+port = "14893"
+database_name = "defaultdb"
+database_address = "mysql-a014630-project-159c.d.aivencloud.com"
 
 engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{database_address}:{port}/{database_name}")
 
